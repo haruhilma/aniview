376c48570a3c24ba99996212abac58a0748df465
haruhilma <jojobarairuma@gmail.com>
2020-12-05 21:27:28 +0700

    setting db

--- aniview/settings.py
+++ aniview/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'AniviewApp'
+    'AniviewApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'aniview.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'aniview',
+        'USER' : 'root',
+        'PASSWORD' : '',
+        'HOST' : 'localhost',
+        'PORT' : '3306'
+
     }
 }
 
